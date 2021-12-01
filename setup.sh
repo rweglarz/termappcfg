513eede8b0b39f23efe15f40cdbe14e403f6f300
Rafal Weglarz <rafal.weglarz@gmail.com>
2021-12-01 21:22:44 +0100

    Create links for kubectl commands

--- setup.sh
+++ setup.sh
@@ -15,3 +15,15 @@ for fcfg in p10k.zsh tmux.conf zshrc; do
 	fi
 	ln -s ${cfg_folder}/${fcfg} ~/.${fcfg} && echo linked ...
 done
+for ks in bin/*; do
+	echo -n ${ks} ...
+	if [ -h ~/${ks} ]; then
+		echo is already linked
+		continue
+	fi
+	if [ -f ~/${ks} ]; then
+		echo is a file, please verify, remove and rerun
+		exit 1
+	fi
+	ln -s ${cfg_folder}/${ks} ~/${ks} && echo linked ...
+done
